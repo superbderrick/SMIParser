8b24fb0a5ee8b839002148ec2a9b674a58827ccf
Derrick <kang.derrick@gmail.com>
2016-12-26 21:15:13 +0900

    created libary project

---
+++ SMIParser/SMIParser/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SMIParser
+//
+//  Created by Kang Jinyeoung on 2016. 12. 26..
+//  Copyright © 2016년 SuperbDerrick. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+    return 0;
+}
