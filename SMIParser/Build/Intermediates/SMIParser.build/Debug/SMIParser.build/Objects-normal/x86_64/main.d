16bfc45090d5c3f990e082aa0502788a564934c4
Derrick <kang.derrick@gmail.com>
2016-12-28 21:47:44 +0900

    added test smi file and added test code to check reading of textfile.

---
+++ SMIParser/Build/Intermediates/SMIParser.build/Debug/SMIParser.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,1 @@
+dependencies: /Users/derrick/latest/SMIParser/SMIParser/SMIParser/main.c
--- SMIParser/SMIParser/main.c
+++ SMIParser/SMIParser/main.c
@@ -9,6 +9,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_LENGTH 1048576
+int test = 0;
 int main(int argc, const char * argv[]) {
   
   if (argc != 2) {
@@ -19,10 +21,33 @@ int main(int argc, const char * argv[]) {
     printf("Start Parsing AT >>>>>>>>>>>> %s <<<<<<<<<<  \n" , argv[1]);
     printf("--------------------------------------------- \n");
     
+    FILE* fin;
+    char line[MAX_LENGTH]; /* 라인 문자열을 담들 버퍼 */
+    
+    
+    fin = fopen(argv[1], "r" );
+    
+    
+    if( fin == NULL )
+    {
+      exit( EXIT_FAILURE );
+    }
+    
+    while( fgets( line, MAX_LENGTH, fin ) )
+    {
+      test++;
+      fputs( line, stdout );
+    }
+    
+    fclose( fin );
+    
+    
+    
+    
     
   }
-    
   
+  printf("%d\n", test);
   
     return 0;
 }
