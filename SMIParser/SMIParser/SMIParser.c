aa85c4b4a07e2eaeeaf2ecd695ed6d83dbc601e1
Derrick <kang.derrick@gmail.com>
2016-12-31 15:09:22 +0900

    update test sample

---
+++ SMIParser/SMIParser/SMIParser.c
@@ -0,0 +1,9 @@
+//
+//  SMIParser.c
+//  SMIParser
+//
+//  Created by Kang Jinyeoung on 2016. 12. 31..
+//  Copyright © 2016년 SuperbDerrick. All rights reserved.
+//
+
+#include "SMIParser.h"
---
+++ SMIParser/SMIParser/SMIParser.h
@@ -0,0 +1,14 @@
+//
+//  SMIParser.h
+//  SMIParser
+//
+//  Created by Kang Jinyeoung on 2016. 12. 31..
+//  Copyright © 2016년 SuperbDerrick. All rights reserved.
+//
+
+#ifndef SMIParser_h
+#define SMIParser_h
+
+#include <stdio.h>
+
+#endif /* SMIParser_h */
