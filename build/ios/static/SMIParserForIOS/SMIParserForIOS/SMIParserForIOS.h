cc54dc92fd73f89c81b5d6f2255f7169420ac33b
Derrick <kang.derrick@gmail.com>
2017-01-06 21:00:49 +0900

    libary project added for iOS

---
+++ build/ios/static/SMIParserForIOS/SMIParserForIOS/SMIParserForIOS.h
@@ -0,0 +1,13 @@
+//
+//  SMIParserForIOS.h
+//  SMIParserForIOS
+//
+//  Created by Kang Jinyeoung on 2017. 1. 6..
+//  Copyright © 2017년 SuperbDerrick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SMIParserForIOS : NSObject
+
+@end
---
+++ build/ios/static/SMIParserForIOS/SMIParserForIOS/SMIParserForIOS.m
@@ -0,0 +1,13 @@
+//
+//  SMIParserForIOS.m
+//  SMIParserForIOS
+//
+//  Created by Kang Jinyeoung on 2017. 1. 6..
+//  Copyright © 2017년 SuperbDerrick. All rights reserved.
+//
+
+#import "SMIParserForIOS.h"
+
+@implementation SMIParserForIOS
+
+@end
