94f729cb7fc48d0eaa0951b519bed4e78a494e98
Derrick <kang.derrick@gmail.com>
2017-01-28 17:14:02 +0900

    refactoring code and replace datastrucure to linkdlist

--- SMIParser/SMIParser/SMIProcessor.c
+++ SMIParser/SMIParser/SMIProcessor.c
@@ -8,33 +8,31 @@
 
 #include "SMIProcessor.h"
 #include "CaptionDataStrucure.h"
+
 int checkStartProcessingPoint(char * strings);
 void startProcessing(char * strings , captionInformation * info);
 
 captionInformation* getStartTime(char * strings , captionInformation * info);
-captionInformation* getText(char * strings);
-captionInformation* getEndTime(char * strings);
 
 int captionStatus = CAPTION_READY;
 
-void initProcessor(char ** captionArray) {
-  int processingStatus = NOT_YET;
-  for (int i = 0 ; captionArray[i] != NULL ; i++) {
-    //if you want to get met informations from SMI file please delete comments.
+void checkEntryPoint(int *processingStatus_p, int i, char **captionArray) {
     //showMetaTagInformations(captionArray[i]) ;
    
-    if(processingStatus == NOT_YET) {
+      if(*processingStatus_p == NOT_YET) {
       if(checkStartProcessingPoint(captionArray[i]) == SEEK_BODY_TAG) {
-        processingStatus = SEEK_BODY_TAG;
+        *processingStatus_p = SEEK_BODY_TAG;
       } else if (captionArray[i] == NULL) {
-        processingStatus = NOTHING_BODY_TAG;
+        *processingStatus_p = NOTHING_BODY_TAG;
       }else {
-        processingStatus = NOT_YET;
+        *processingStatus_p = NOT_YET;
       }
     }
-    
-    if(processingStatus == NOT_YET) {
-      continue;
+}
+
+void analzecaptiondata(int processingStatus, int i, char **captionArray) {
+  if(processingStatus == NOT_YET) {
+      return;
     } else if(processingStatus == SEEK_BODY_TAG) {
       captionInformation * info;
       startProcessing(captionArray[i] , info);
@@ -42,8 +40,14 @@ void initProcessor(char ** captionArray) {
       printf("UnSupportType ,it can't looking for <BODY> Tag  \n");
       exit(1);
     }
+}
+
+void initProcessor(char ** captionArray) {
+  int processingStatus = NOT_YET;
+  for (int i = 0 ; captionArray[i] != NULL ; i++) {
+    checkEntryPoint(&processingStatus, i, captionArray);
     
-    
+    analzecaptiondata(processingStatus, i, captionArray);
     
   }
   printf("ProcessingDone \n" );
@@ -74,49 +78,45 @@ captionInformation* getStartTime(char * strings ,captionInformation * info) {
   
   return info;
 }
-captionInformation* getText(char * strings) {
-  return NULL;
-}
-captionInformation* getEndTime(char * strings) {
-  return NULL ;
-}
+
 
 void startProcessing(char * strings ,captionInformation * info) {
+  printf("test line by line %s \n" , strings );
   const static char * startPart = "<SYNC Start=";
   
-  if(captionStatus == CAPTION_READY) {
-    if(strstr(strings, startPart) != NULL) {
-      captionStatus = CAPTION_MAKING;
-      
-      
-      info = getStartTime(strings , info);
-      
-    }
-  } else if(captionStatus == CAPTION_MAKING) {
-    char *pptr;
-    int k = 0;
-    
-//    while(pptr != NULL ){
-//      if(k == 0) {
-//        pptr = strtok(strings, "=");
-//        printf( "%s\n" , pptr);
-//        pptr = strtok(NULL, ">");
-//        info.fontColor = pptr;
-//        printf( "%s\n" , pptr);
-//      } else if(k == 1) {
-//        pptr = strtok(NULL, "<");
-//        printf( "%s\n" , pptr);
-//        info.captionText = pptr;
-//        break;
-//      }
+//  if(captionStatus == CAPTION_READY) {
+//    if(strstr(strings, startPart) != NULL) {
+//      captionStatus = CAPTION_MAKING;
 //      
-//      k++;
+//      
+//      info = getStartTime(strings , info);
+//      
 //    }
-    
-  } else if (captionStatus == CAPTION_COMPLETE) {
-    
-  }
-  
+//  } else if(captionStatus == CAPTION_MAKING) {
+//    char *pptr;
+//    int k = 0;
+//    
+////    while(pptr != NULL ){
+////      if(k == 0) {
+////        pptr = strtok(strings, "=");
+////        printf( "%s\n" , pptr);
+////        pptr = strtok(NULL, ">");
+////        info.fontColor = pptr;
+////        printf( "%s\n" , pptr);
+////      } else if(k == 1) {
+////        pptr = strtok(NULL, "<");
+////        printf( "%s\n" , pptr);
+////        info.captionText = pptr;
+////        break;
+////      }
+////      
+////      k++;
+////    }
+//    
+//  } else if (captionStatus == CAPTION_COMPLETE) {
+//    
+//  }
+//  
 }
 
 int checkStartProcessingPoint(char * strings ) {
